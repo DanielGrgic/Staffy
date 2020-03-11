da800cb4361537e13a52683e6b9969e8aba1832e
DanielGrgic <daniel.grgic.123@gmail.com>
2020-03-11 09:05:44 +0100

    TopBar

---
+++ src/components/TopBar/TopBar.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import logoSvg from '../../logo.svg';
+
+const Container = styled.div`
+  display: flex;
+  background: #F4F6F8;
+  box-shadow: inset 0px -1px 0px #DFE3E8;
+  height: 56px;
+  padding: 16px 32px;
+  justify-content: space-between;
+  align-items: center;  
+`
+
+const RightText = styled.p`
+  margin: 0;
+  padding: 0;
+  font-family: 'SF Pro Display', sans-serif;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 15px;
+  line-height: 20px;
+  color: #637381;
+`
+
+const TopBar = () => {
+  return (
+    <Container>
+      <img src={logoSvg} />
+      <RightText>by Staffy</RightText>
+    </Container>
+  )
+}
+
+export default TopBar;
