3cdda3bc5582b23d7f03c77722bd4b37e9b83b98
DanielGrgic <daniel.grgic.123@gmail.com>
2020-03-13 10:14:55 +0100

    static step 2 ~ 5

--- src/components/RightSection/StepSection.js
+++ src/components/RightSection/StepSection.js
@@ -1,27 +1,87 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styled from 'styled-components';
 import Form from 'react-bootstrap/Form'
 import Select from 'react-select';
+import Button from 'react-bootstrap/Button'
 
 import { SELECT_OPTIONS } from '../../constants';
 
 const StepSection = ({
   step,
 }) => {
+
+  const getTitle = (step=1) => {
+    switch(step) {
+      case 1:
+        return 'Let’s get to know you and your Shopify store a bit:';
+      case 2:
+      case 3:
+        return "Well let's continue";
+      default: 
+        return 'Let’s get to know you and your Shopify store a bit:';
+    }
+  }  
+
   return (
     <Container>
-      <Title>Let’s get to know you and your Shopify store a bit:</Title>
+      {
+        step < 6 && (
+          <Title>{getTitle()}</Title>
+        )
+      }      
+      {
+        (step === 2 || step === 3 || step ===4) && (
+          <SubTitle style={{marginTop: '24px'}}>Let's get to know you andyour Shopify store a bit:</SubTitle>
+        )
+      }
       <Form>
-        <Form.Group style={{marginTop: '24px'}}>
-          <StepFormLabel>Why do you need staffy?</StepFormLabel>
-          <StepSelect
-            placeholder="Choose Multiple"
-            options={SELECT_OPTIONS}
-            isMulti={true}                    
-          />              
-        </Form.Group>                      
+        {
+          (step <= 5) && (      
+            <Form.Group style={{marginTop: '24px'}}>
+            <StepFormLabel>Why do you need staffy?</StepFormLabel>
+              <StepSelect
+                placeholder="Choose Multiple"
+                options={SELECT_OPTIONS}
+                isMulti={true}
+              />              
+            </Form.Group>                      
+          )
+        }
+        {
+          (step > 1 && step <= 5) && (          
+            <Form.Group style={{marginTop: '24px'}}>
+              <StepFormLabel>How many people do you need to manage?</StepFormLabel>
+              <StepSelect
+                placeholder="Choose One"
+                options={SELECT_OPTIONS}
+              />              
+            </Form.Group>
+          )
+        }
+        {
+          (step > 2 && step <= 5) && (          
+            <Form.Group style={{marginTop: '24px'}}>
+              <StepFormLabel>What type of Shopify store do you have?</StepFormLabel>
+              <StepSelect
+                placeholder="Choose One"
+                options={SELECT_OPTIONS}
+              />              
+            </Form.Group>            
+          )
+        }
+        {
+          (step > 3 && step <= 5) && (          
+            <Form.Group style={{marginTop: '24px'}}>
+              <StepFormLabel>What do your staff do?</StepFormLabel>
+              <StepSelect
+                placeholder="Choose One"
+                options={SELECT_OPTIONS}
+              />              
+            </Form.Group>            
+          )
+        }
       </Form>
-    </Container>  
+    </Container>
   )
 }
 
@@ -36,11 +96,21 @@ const Container = styled.div`
 const Title = styled.h1`
   margin: 0;
   padding: 0;
+  font-family: 'SF Pro Display', sans-serif;
   font-style: normal;
   font-weight: 300;
   font-size: 28px;
   line-height: 32px;
   color: #13123C;
+`
+
+const SubTitle = styled.h2`
+  font-family: 'SF Pro Display', sans-serif;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 20px;
+  line-height: 28px;
+  color: #8E9FAD;
 `
 
 const StepFormLabel = styled(Form.Label)`
@@ -57,6 +127,39 @@ const StepFormLabel = styled(Form.Label)`
 
 const StepSelect = styled(Select)`
   width: 331px;
+`
+
+const NextBtn = styled(Button)`
+  background-color: #F34867;
+  border-radius: 3px;
+  font-family: 'SF Pro Display', sans-serif;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 14px;
+  line-height: 20px;    
+  color: #FFFFFF;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: none !important;
+  width: 80px;
+  height: 36px;
+  outline: none !important;
+  box-shadow: none !important;
+  margin-top: 24px;
+
+  &:hover {
+    background-color: #F34867 !important;
+    opacity: 0.8;
+  }
+  &:active {
+    background-color: #F34867 !important;
+    opacity: 0.8;
+  }  
+  &:focus {
+    background-color: #F34867 !important;
+    opacity: 0.8;
+  }  
 `
 
 export default StepSection;
